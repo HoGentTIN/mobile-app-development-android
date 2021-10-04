a59eb3e64389c95caa4e968d6a7da25e08f5e40e
Gertjan Bosteels <gertjan.bosteels@hogent.be>
2021-10-04 19:55:23 +0200

    adds random joke funcitonality

---
+++ JokeApp/app/src/main/java/com/example/jokeapp/JokeBook.kt
@@ -0,0 +1,20 @@
+package com.example.jokeapp
+
+import kotlin.random.Random
+
+class JokeBook {
+    val jokes = listOf(
+        "My wife said I should do lunges to stay in shape. That would be a big step forward.",
+        "I thought the dryer was shrinking my clothes. Turns out it was the refrigerator all along",
+        "I only know 25 letters of the alphabet. I don't know y.",
+        "I asked my dog what's two minus two. He said nothing.",
+        "This graveyard looks overcrowded. People must be dying to get in.",
+        "I have a joke about chemistry, but I don't think it will get a reaction.",
+        "I used to be addicted to soap, but I'm clean now."
+    )
+
+    fun getRandomJoke() : String {
+        var randomListNumber = Random.nextInt(jokes.size)
+        return jokes[randomListNumber]
+    }
+}
--- JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.kt
+++ JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.jokeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var myJokeBook : JokeBook
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myJokeBook = JokeBook()
+        val myButton : Button = findViewById(R.id.nextjoke_button)
+        myButton.setOnClickListener{
+            changeJoke()
+        }
     }
+
+    private fun changeJoke() {
+        val jokeText: TextView = findViewById(R.id.joke_textview)
+        jokeText.text = myJokeBook.getRandomJoke()
+    }
+
 }
