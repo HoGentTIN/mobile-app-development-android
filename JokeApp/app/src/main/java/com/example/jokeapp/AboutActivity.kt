ce96f8a97f1861bc5f864ef89e394f7702b672bc
Gertjan Bosteels <gertjan.bosteels@hogent.be>
2021-10-11 17:30:27 +0200

    adds about page with scrollable text

---
+++ JokeApp/app/src/main/java/com/example/jokeapp/AboutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jokeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_me)
+    }
+}
