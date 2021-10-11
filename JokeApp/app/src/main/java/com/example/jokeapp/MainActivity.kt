9f8517c56f8eee972226adc655943fbd1bab1ef0
Gertjan Bosteels <gertjan.bosteels@hogent.be>
2021-10-11 13:24:38 +0200

    adds databinding and refactors string resources

--- JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.kt
+++ JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.kt
@@ -2,52 +2,62 @@ package com.example.jokeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.jokeapp.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    lateinit var myJokeBook : JokeBook
+    private lateinit var myJokeBook : JokeBook
 
-    lateinit var myButton: Button
-    lateinit var happyButton: Button
-    lateinit var jokeText: TextView
-    lateinit var happyImage: ImageView
+    /*private lateinit var myButton: Button
+    private lateinit var happyButton: Button
+    private lateinit var jokeText: TextView
+    private lateinit var happyImage: ImageView*/
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         myJokeBook = JokeBook()
-        myButton = findViewById(R.id.nextjoke_button)
-        happyButton = findViewById(R.id.happy_button)
-        jokeText = findViewById(R.id.joke_textview)
-        happyImage = findViewById(R.id.happy_image)
 
-        myButton.setOnClickListener{
-            changeJoke()
-        }
+        setOnClickListeners()
 
-        happyButton.setOnClickListener{
-            happy()
+    }
+
+    //set clicklistener for each element in the list
+    private fun setOnClickListeners() {
+        var clickableElements = listOf(
+            binding.happyButton,
+            binding.nextjokeButton
+        )
+        for (item in clickableElements){
+            when(item.id){
+                R.id.nextjoke_button -> item.setOnClickListener { changeJoke() }
+                R.id.happy_button -> item.setOnClickListener { happy() }
+            }
         }
     }
 
     private fun happy() {
-        var randomInt = Random.nextInt(3)
-        val drawableResource = when (randomInt) {
+        val drawableResource = when (Random.nextInt(3)) {
             0 -> R.drawable.ic_iconmonstr_smiley_1
             1 -> R.drawable.ic_iconmonstr_smiley_13
             else -> R.drawable.ic_iconmonstr_smiley_2
 
         }
-        happyImage.setImageResource(drawableResource)
+        binding.happyImage.setImageResource(drawableResource)
     }
 
     private fun changeJoke() {
-        jokeText.text = myJokeBook.getRandomJoke()
+        binding.jokeTextview.text = myJokeBook.getRandomJoke()
     }
 
 }
