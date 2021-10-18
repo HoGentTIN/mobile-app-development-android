31042d81c2e05af75c9fb40bd6402f5675d578aa
Gertjan Bosteels <gertjan.bosteels@hogent.be>
2021-10-18 08:52:28 +0200

    adds overflow menu

---
+++ JokeApp/app/src/main/java/com/example/jokeapp/AboutFragment.kt
@@ -0,0 +1,33 @@
+package com.example.jokeapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AboutFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AboutFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+
+}
--- JokeApp/app/src/main/java/com/example/jokeapp/HomeFragment.kt
+++ JokeApp/app/src/main/java/com/example/jokeapp/HomeFragment.kt
@@ -1,14 +1,13 @@
 package com.example.jokeapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.jokeapp.databinding.FragmentHomeBinding
 import kotlin.random.Random
 
@@ -36,7 +35,19 @@ class HomeFragment : Fragment() {
 
         setOnClickListeners()
 
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
     private fun setOnClickListeners() {
