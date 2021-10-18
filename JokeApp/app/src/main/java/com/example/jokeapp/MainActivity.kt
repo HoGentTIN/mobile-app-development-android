8939266b6e88d9be23e27e991a22e7e06701b7a4
Gertjan Bosteels <gertjan.bosteels@hogent.be>
2021-10-18 08:43:12 +0200

    adds back button support

--- JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.kt
+++ JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.jokeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +13,15 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         setContentView( R.layout.activity_main)
 
+        val navControler = this.findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navControler)
 
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
+    }
 
 
 }
