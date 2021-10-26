2c767068743d5214d8936203f7c3447101b3f5ee
Gertjan Bosteels <gertjan.bosteels@hogent.be>
2021-10-26 13:20:44 +0200

    adds button navigation in landscape mode the propper way

--- JokeApp/app/src/main/java/com/example/jokeapp/ButtonNavigationFragment.kt
+++ JokeApp/app/src/main/java/com/example/jokeapp/ButtonNavigationFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.jokeapp.databinding.FragmentButtonNavigationBinding
 
 
 /**
@@ -20,13 +23,17 @@ class ButtonNavigationFragment : Fragment() {
 
     }
 
+    lateinit var binding : FragmentButtonNavigationBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_button_navigation, container, false)
-
+        //return inflater.inflate(R.layout.fragment_button_navigation, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_button_navigation, container, false)
+        binding.button2.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment2_to_jokeFragment))
+        return binding.root
     }
 
 
