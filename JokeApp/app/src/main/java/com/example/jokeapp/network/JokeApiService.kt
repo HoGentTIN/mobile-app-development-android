02330ac478d34af4e4fc14ccaa1ae3b9ef02c490
Gertjan Bosteels <gertjan.bosteels@hogent.be>
2021-11-30 08:31:27 +0100

    adds http logger for OkHttp

--- JokeApp/app/src/main/java/com/example/jokeapp/network/JokeApiService.kt
+++ JokeApp/app/src/main/java/com/example/jokeapp/network/JokeApiService.kt
@@ -4,6 +4,8 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -19,12 +21,17 @@ private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
+private val logger = HttpLoggingInterceptor().apply{level = HttpLoggingInterceptor.Level.BASIC}
 
+private val client = OkHttpClient.Builder()
+    .addInterceptor(logger)
+    .build()
 //Scalars Converter = converter for strings to plain text bodies
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(BASE_URL)
+    .client(client)
     .build()
 
 /*private val retrofit = Retrofit.Builder()
