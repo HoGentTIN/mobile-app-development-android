d69f3a875a0b53cdc574f96912f0aaa9c5822c61
Jan Willem <102214064+jw-hogent@users.noreply.github.com>
2023-10-24 12:00:17 +0200

    Scroll to new item on add

--- app/src/main/java/com/example/taskapp/ui/TaskOverview.kt
+++ app/src/main/java/com/example/taskapp/ui/TaskOverview.kt
@@ -3,11 +3,15 @@ package com.example.taskapp.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun TaskOverview(
@@ -19,9 +23,17 @@ fun TaskOverview(
     val taskOverviewState by taskOverviewViewModel.uiState.collectAsState()
 
     Box(modifier = modifier) {
-        LazyColumn() {
+        val lazyListState = rememberLazyListState()
+        LazyColumn(state = lazyListState) {
             items(taskOverviewState.currentTaskList) {
                 TaskItem(name = it.name, description = it.description)
+            }
+        }
+        val coroutineScope = rememberCoroutineScope()
+
+        LaunchedEffect(taskOverviewState.currentTaskList.size) {
+            coroutineScope.launch {
+                lazyListState.animateScrollToItem(taskOverviewState.currentTaskList.size - 1)
             }
         }
 
