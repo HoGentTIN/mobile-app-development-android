9a22bae9b832017317f125117df58f9a66f0c5e1
Jan Willem <102214064+jw-hogent@users.noreply.github.com>
2023-10-24 11:59:18 +0200

    Restyle TaskItem, add animations

--- app/src/main/java/com/example/taskapp/ui/TaskItem.kt
+++ app/src/main/java/com/example/taskapp/ui/TaskItem.kt
@@ -1,5 +1,10 @@
 package com.example.taskapp.ui
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -7,14 +12,32 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Checkbox
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.RadioButtonChecked
+import androidx.compose.material.icons.filled.RadioButtonUnchecked
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.taskapp.R
+import com.example.taskapp.ui.theme.TaskAppTheme
 
 @Composable
 fun TaskItem(
@@ -22,26 +45,108 @@ fun TaskItem(
     name: String = "",
     description: String = "",
 ) {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .height(IntrinsicSize.Min)
-            .fillMaxWidth(),
+    Card(
+        modifier = modifier.padding(dimensionResource(R.dimen.padding_small)),
     ) {
-        Column(
-            modifier = Modifier.padding(8.dp),
+        var expanded by remember { mutableStateOf(false) }
+        val color by animateColorAsState(
+            targetValue = if (expanded) {
+                MaterialTheme.colorScheme.tertiaryContainer
+            } else {
+                MaterialTheme.colorScheme.secondaryContainer
+            },
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+//            verticalAlignment = Alignment.Top,
+            modifier = modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium,
+                    ),
+                ).height(IntrinsicSize.Min)
+                .fillMaxWidth()
+                .background(color),
         ) {
-            Text(
-                text = name,
-                fontSize = 24.sp,
+            var done by remember { mutableStateOf(false) }
+            Column(
+                modifier = Modifier
+                    .padding(8.dp),
+            ) {
+                Row {
+                    TaskItemStatus(
+                        done,
+                        onClick = { done = !done },
+                    )
+                    Text(
+                        text = name,
+                        //                    fontSize = 24.sp,
+                        style = MaterialTheme.typography.headlineLarge,
+                        textDecoration = if (done) TextDecoration.LineThrough else TextDecoration.None,
+                    )
+                }
+                if (expanded) {
+                    Text(
+                        text = description,
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily.Monospace,
+                    )
+                }
+            }
+            TaskItemButton(
+                expanded = expanded,
+                onClick = { expanded = !expanded },
+                modifier = Modifier.align(Alignment.Top),
             )
-            Text(
-                text = description,
-                fontSize = 14.sp,
-                fontFamily = FontFamily.Monospace,
-            )
+//            Checkbox(checked = false, onCheckedChange = {})
         }
-        Checkbox(checked = false, onCheckedChange = {})
+    }
+}
+
+@Composable
+fun TaskItemStatus(
+    done: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier,
+    ) {
+        Icon(
+            imageVector = if (done) Icons.Filled.RadioButtonChecked else Icons.Filled.RadioButtonUnchecked,
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary,
+        )
+    }
+}
+
+@Composable
+fun TaskItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier,
+    ) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TaskItemPreview() {
+    TaskAppTheme {
+        TaskItem(
+            name = "Name of item",
+            description = "Description of task",
+        )
     }
 }
