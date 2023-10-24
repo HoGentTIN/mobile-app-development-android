317da44f7dbc22595b7e2402fdab98788bed7fa0
Jan Willem <102214064+jw-hogent@users.noreply.github.com>
2023-10-24 11:44:54 +0200

    Add font

--- app/src/main/java/com/example/taskapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/taskapp/ui/theme/Type.kt
@@ -2,9 +2,20 @@ package com.example.taskapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.taskapp.R
+
+/*
+Go to https://fonts.google.com/, search for Amatic and download.
+Create res/font, put the bold font inside and rename: all lower case
+and underscore instead of dash.
+ */
+val AmaticSCBold = FontFamily(
+    Font(R.font.amaticsc_bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -13,8 +24,25 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+        letterSpacing = 0.5.sp,
+    ),
+    /*
+    Look at the fontSize you are currently using in your design, and
+    find the right size in:
+    https://m3.material.io/styles/typography/type-scale-tokens#40fc37f8-3269-4aa3-9f28-c6113079ac5d
+     */
+    headlineLarge = TextStyle(
+        fontFamily = AmaticSCBold,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = AmaticSCBold,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
